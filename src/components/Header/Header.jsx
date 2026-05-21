1fcb8e8737eee4f69ef0ebf55ecf26280527a548
MARIO USANGA <mariobomausanga@gmail.com>
2026-05-21 14:32:25 +1000

    Update header text and role description

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
             <Navbar />
             <div className='heading'>
             <p className='heading-p1'>Hello there, my name is <span>MARIO</span></p>
-            <h1 className='heading-h'>Creating Websites as Unique as Your Brand.</h1>
-            <p className='heading-p2'>I’m a <span>front-end developer</span> & <span>web designer</span></p>
+            <h1 className='heading-h'>Full-Stack Solutions as Unique as Your Brand.</h1>
+            <p className='heading-p2'>I’m a <span>Software Engineer</span></p>
             <a href="#about" className='more-btn'>See More</a>
             </div>
         </div>
