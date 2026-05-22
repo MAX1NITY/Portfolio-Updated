0610d41aef1a89b2e1f8698ea16174206b6ab9f4
MARIO USANGA <mariobomausanga@gmail.com>
2026-05-22 11:30:05 +1000

    Update About section to reflect new role and focus

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section id='about' className='about'>
       <div className='wrapper'>
         <h1 className='about-h'>ABOUT</h1>
-        <p className='about-p'>Hi, I'm <span>Mario</span>, a passionate <span>web designer</span> dedicated to crafting unique and engaging digital experiences. With a keen eye for detail and a commitment to excellence, I transform ideas into stunning, functional <span>websites</span> that resonate with users and elevate brands. My journey in <span>web design</span> is driven by a love for creativity and technology, blending aesthetics with seamless functionality. Let's bring your <span>vision</span> to life and create something extraordinary together.</p>
+        <p className='about-p'>Hi, I'm <span>Mario</span>, a passionate <span>Software Developer</span> dedicated to crafting unique and engaging digital experiences. With a keen eye for detail and a commitment to excellence, I transform ideas into functional <span>programs</span> that resonate with users and elevate brands. My journey in <span>Software Engineering</span> is driven by a love for solving complex problems. Let's bring your <span>vision</span> to life and create something extraordinary together.</p>
       </div>
     </section>
   )
